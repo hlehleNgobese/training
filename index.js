4560f69d78dda459b4189008f36fcc817a7788aa
Nobuhle <nobuhlengobese0713@gmail.com>
2023-06-20 17:45:07 +0200

    Created express application with routes

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express'); 
+
+const app = express(); 
+
+app.get('/', function(req, res){
+    res.json({'hello': 'World'})
+})
+
+app.get('/nobuhle', function(req,res){
+    res.json({"hello": "Nobuhle"}); 
+})
+
+app.get('/app', function(req, res){
+    res.json({'app': 'here'})
+})
+
+app.get('/gideon', function(req, res){
+    res.json({"Hello ": "Gideon"}); 
+})
+
+app.listen(3000, function(){
+    console.log('app now running on port 3000'); 
+})
+
+
+
+
+
